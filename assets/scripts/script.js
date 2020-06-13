3a4528e82f773fcdadd4f3f9f7a09317d87256fb
theendisfar <r3steve@gmail.com>
2020-06-12 23:42:31 -0400

    reset

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,70 +14,115 @@ var interval;
 var arrayOfQuestions = [
     {
         question: "Question 1",
-        answer1: "Answer 1",
-        answer2: "Answer 2",
-        answer3: "Answer 3",
-        answer4: "Answer 4",
+        answerArray: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
         correctAnswer: 1,
     },
     {
         question: "Question 2",
-        answer1: "Answer 1",
-        answer2: "Answer 2",
-        answer3: "Answer 3",
-        answer4: "Answer 4",
+        answerArray: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
         correctAnswer: 1,
     },
     {
         question: "Question 3",
-        answer1: "Answer 1",
-        answer2: "Answer 2",
-        answer3: "Answer 3",
-        answer4: "Answer 4",
+        answerArray: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
         correctAnswer: 1,
     },
     {
         question: "Question 4",
-        answer1: "Answer 1",
-        answer2: "Answer 2",
-        answer3: "Answer 3",
-        answer4: "Answer 4",
+        answerArray: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
         correctAnswer: 1,
     },
     {
         question: "Question 5",
-        answer1: "Answer 1",
-        answer2: "Answer 2",
-        answer3: "Answer 3",
-        answer4: "Answer 4",
+        answerArray: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correctAnswer: 1,
     },
 ];
 
 
 
+questionNumber = 0;
 // Define functions
 function startQuiz() {
-    console.log("1st Question");
+
+    console.log("Start Timer and Create 1st Question");
+    //Start Timer
+    // Clear Div
     mainEl.innerHTML = "";
-    var currentQuestion = arrayOfQuestions[currentQuestionIndex];
-    var displayedQuestion = document.createElement("h2");
-    displayedQuestion.textContent = currentQuestion.question;
-    mainEl.append(displayedQuestion);
+
+    // Create element h2 and textContent Question
+    var questionEl = document.createElement("h2");
     var answerList = document.createElement("ul");
+
+    questionEl.textContent = arrayOfQuestions[questionNumber].question;
+    mainEl.append(questionEl);
     mainEl.append(answerList);
-    var answerItem1 = document.createElement("li");
-    answerItem1.textContent = currentQuestion.answer1;
-    answerList.append(answerItem1);
-    var answerItem2 = document.createElement("li");
-    answerItem2.textContent = currentQuestion.answer2;
-    answerList.append(answerItem2);
-    var answerItem3 = document.createElement("li");
-    answerItem3.textContent = currentQuestion.answer3;
-    answerList.append(answerItem3);
-    var answerItem4 = document.createElement("li");
-    answerItem4.textContent = currentQuestion.answer4;
-    answerList.append(answerItem4);
-    console.log(currentQuestion.correctAnswer);
+    for (var i = 0; i < arrayOfQuestions[questionNumber].answerArray.length; i++) {
+        var answerEl = document.createElement("li");
+        answerEl.textContent = arrayOfQuestions[questionNumber].answerArray[i];
+        answerEl.setAttribute("data-index", i);
+        answerList.append(answerEl);
+
+
+
+        
+
+
+
+    }
+    // Var for question number
+    // Create Elements for Answers and textContent each
+    // Add event listener to capture button clicked
+    // if button clicked === answer 
+      // Display correct 
+      // increment question number
+      //  startQuiz
+      // else
+        // Display Wrong
+        // Increment Quesiton number
+        // subract 10 seconds
+        // startQuiz
+
+
+        
+    
+}
+
+startBtn.addEventListener("click", startQuiz)
+mainEl.addEventListener("click", function(event) {
+    var element = event.target;
+    if(element.matches("li") === true) {
+        var index = element.ParentElement.getAttribute("data-index");
+        console.log("data-index");
+    }
+
+});
+
+
+
+
+
+
+    // var currentQuestion = arrayOfQuestions[currentQuestionIndex];
+    // var displayedQuestion = document.createElement("h2");
+    // displayedQuestion.textContent = currentQuestion.question;
+    // mainEl.append(displayedQuestion);
+    // var answerList = document.createElement("ul");
+    // answerList.setAttribute("style", "list-style-type: none")
+    // mainEl.append(answerList);
+    // var answerItem1 = document.createElement("li"); 
+    // answerItem1.textContent = currentQuestion.answer1;
+    // answerList.append(answerItem1);
+    // var answerItem2 = document.createElement("li");
+    // answerItem2.textContent = currentQuestion.answer2;
+    // answerList.append(answerItem2);
+    // var answerItem3 = document.createElement("li");
+    // answerItem3.textContent = currentQuestion.answer3;
+    // answerList.append(answerItem3);
+    // var answerItem4 = document.createElement("li");
+    // answerItem4.textContent = currentQuestion.answer4;
+    // answerList.append(answerItem4);
+    // console.log(currentQuestion.correctAnswer);
 
 
     // var answerOne = document.createElement("button");
@@ -98,7 +143,7 @@ function startQuiz() {
 
 
 
-}
+
 
 
 
@@ -110,4 +155,3 @@ function startQuiz() {
 
 
 // Event Listeners
-startBtn.addEventListener("click", startQuiz)
