376c20ddc2d50f2bd3e20af38b38a39c70e3812f
theendisfar <r3steve@gmail.com>
2020-06-14 16:07:16 -0400

    Hard coded Start Screen, logged event listener for start button

--- assets/scripts/script2.js
+++ assets/scripts/script2.js
@@ -1,2 +1,19 @@
 console.log("JS is linked");
 
+// Variables
+var startBtn = document.getElementById("start-button");
+
+
+
+// Functions
+
+
+
+// Function Calls
+
+
+
+// Event Listeners
+startBtn.addEventListener("click", function() {
+    console.log("Start Button Clicked");
+})
